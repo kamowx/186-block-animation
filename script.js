1af1de548114ebe5e3f7f66801e00ec0b28b4b10
kamowx <257101047+kamowx@users.noreply.github.com>
2026-06-06 00:07:07 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+function yellow(){
+    $('.yellow').animate({
+        bottom: '2%',
+        transition: '1s'
+    });
+}
+
+setTimeout(yellow, 1000);
+
+
+function oval(){
+    $('.oval').animate({
+        bottom: '0%',
+        transition: '1s'
+    });
+}
+
+setTimeout(oval, 2000);
+
+
+function diamond(){
+    $('.diamond').animate({
+        bottom: '20px',
+        transition: '1s'
+    });
+}
+
+setTimeout(diamond, 3000);
+
+
+function rect(){
+    $('.rect').animate({
+        bottom: '76px',
+        transition: '1s'
+    });
+}
+
+setTimeout(rect, 4000);
+
+function line(){
+    $('.line').animate({
+        bottom: '105px',
+        transition: '1s'
+    });
+}
+
+setTimeout(line, 5000);
+
+
+function hex(){
+    $('.hex').animate({
+        bottom: '112px',
+        transition: '1s'
+    });
+}
+
+setTimeout(hex, 6000);
+
+function circle(){
+    $('.circle').animate({
+        bottom: '0px',
+        transition: '1s'
+    });
+}
+
+setTimeout(circle, 7000);
